fc4adf27acdff8dba52170386aae3d7f8ceb8a89
Bartosz Gajda <bgajda@red-paint.com>
2017-09-18 16:54:02 +0100

    Blade templating and compiling assets.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,14 +7,21 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index() {
-        return view('pages.index');
+        $title = 'Welcome From Var';
+//        return view('pages.index', compact('title'));
+        return view('pages.index')->with('title', $title);
     }
 
     public function about() {
-        return view('pages.about');
+        $title = 'Welcome From Var';
+        return view('pages.about')->with('title', $title);
     }
     public function services() {
-        return view('pages.services');
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Web', 'Mobile', 'Cloud']
+        );
+        return view('pages.services')->with($data);
     }
 
 }
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,13 +1,12 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ config('app.name', 'Not Found') }}</title>
-</head>
-<body>
-<h1>Welcome to Services Page</h1>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1>Welcome to Services Page</h1>
+    @if(count($services) > 0)
+        <ul>
+        @foreach($services as $service)
+            <li>{{ $service }}</li>
+        @endforeach
+        </ul>
+    @endif
+@endsection
