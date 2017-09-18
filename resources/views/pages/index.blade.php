970a1f8fd2340fbd33716a44f300b6296d414826
Bartosz Gajda <bgajda@red-paint.com>
2017-09-18 17:11:16 +0100

    Last part of templating.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,5 +4,6 @@
     <div class="jumbotron text-center">
         <h1>Welcome to Index Page</h1>
         <h2>{{ $title }}</h2>
+        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
     </div>
 @endsection
